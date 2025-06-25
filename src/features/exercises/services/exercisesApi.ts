de0d7dc4897c7f5f1ba8a42da1321e301c4879df
unknown <piero_gd99@outlook.com>
2025-06-25 12:10:44 -0500

    fix: full backend endpoint & exercises token

--- src/features/exercises/services/exercisesApi.ts
+++ src/features/exercises/services/exercisesApi.ts
@@ -1,9 +1,14 @@
 import type { Exercise } from "../types/exercise";
 
-const API_URL = "https://infinityhealthapi.onrender.com/api/fit-bibliotec/exercises/";
-
 export async function fetchExercises(): Promise<Exercise[]> {
-  const res = await fetch(API_URL);
+  const token = localStorage.getItem('accessToken');
+  const res = await fetch('/api/fit-bibliotec/exercises', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token
+    },
+  });
   if (!res.ok) throw new Error("Error al obtener los ejercicios");
   const data = await res.json();
   console.log("[exercisesApi] fetchExercises data:", data);
