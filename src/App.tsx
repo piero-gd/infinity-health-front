37395f0a2d4f71e39890a905a08d3a43fd324cfe
unknown <piero_gd99@outlook.com>
2025-07-11 15:38:00 -0500

    refactor App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { ToastContainer } from "react-toastify";
 import AppRouter from "./routes/AppRouter";
-import { ErrorBoundary } from "react-error-boundary";
-import { ErrorBoundaryFallback } from "./components/ErrorBoundaryFallback";
-
 function App() {
   const handleLogout = () => {
     console.log("Logout");
